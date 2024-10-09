d6bda4fd0faa77ddd3354113afbd703983cd4031
Matheus Quiles <matheussquiles@gmail.com>
2024-10-09 17:27:44 -0300

    variavel

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://select-data-api-b4ea94b120a3.herokuapp.com/';
+export const API_BASE_URL = 'https://select-data-api-b4ea94b120a3.herokuapp.com/api';
 // export const API_BASE_URL = 'http://localhost:8080/api';
