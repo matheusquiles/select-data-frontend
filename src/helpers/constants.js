10574e733dfb85c69a5462d8614397cb0f9d903f
Matheus Quiles <matheussquiles@gmail.com>
2024-10-09 14:10:12 -0300

    url

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'https://select-data-app-875e0ef5f4ae.herokuapp.com/api';
+// export const API_BASE_URL = 'http://localhost:8080/api';
